e8f8accdcf9ed4947f44799ccd25ff1f5b6b0b31
Joachim Andreault Aiach <joachim.andreault@gmail.com>
2019-08-06 12:44:07 +0200

    tasks exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+get 'tasks', to: 'tasks#index'
+
+  # get 'restaurants/:id', to: 'restaurants#show'
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurant/create', to: 'restaurants#create'
+
+
+  # get 'restaurants/:id/edit', to:'restaurant#edit'
+  # patch 'restaurant/:id',
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
