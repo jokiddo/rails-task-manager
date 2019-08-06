a4137814fcee2defd1de83661f5b1ed376220a2c
Joachim Andreault Aiach <joachim.andreault@gmail.com>
2019-08-06 17:27:13 +0200

    changement

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,48 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+   def update
+    @task.update(tasks_params)
+
+    redirect_to tasks_path
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def edit
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+private
+  def tasks_params
+
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
 get 'tasks', to: 'tasks#index'
+get 'tasks/new', to: 'tasks#new', as: :new_task
+get 'tasks/:id', to: 'tasks#show', as: :task
+post 'tasks', to: 'tasks#create'
+
+get 'tasks/:id/edit', to:'tasks#edit', as: :edit_task
+delete 'tasks/:id/destroy', to:'tasks#destroy', as: :destroy_task
+
+patch 'tasks/:id', to: 'tasks#update'
 
   # get 'restaurants/:id', to: 'restaurants#show'
 
